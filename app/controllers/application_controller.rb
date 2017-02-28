0127017a75791a7d568569c39a7ea8b54b05fb7e
Alexandra Roth <alroth8@gmail.com>
2017-02-28 09:44:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,6 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     if Helpers.is_logged_in?(session)
-      binding.pry
       @user = Helpers.current_user(session)
       erb :account
     else
